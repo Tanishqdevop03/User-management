a915224926149b4c08fdf9243ff017be41c1304c
Tanishq Garg <tanishqtanishq56@gmail.com>
2026-05-11 21:29:48 +0530

    Cors policy

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://user-management-fawn-five.vercel.app/",
+    credentials: true,
+  }
+));
 
 app.use(express.json());
 
